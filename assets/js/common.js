6bd3d4c21c7a15ecd7449e25bad442ae9f4ec379
zzhmeenuya@gmail.com <zzhmeenuya@gmail.com>
2019-05-23 11:45:46 +0400

    проект готов на проверку заказчику

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,6 +9,10 @@ $(function(){
 		$('html, body').animate({
 			scrollTop: sectionOffsetTop - offset
 		}, 1000);
+
+
+		$('.section-cost__title').html('Заказать обратный звонок');
+		$('.form-cost input[name="subject"]').val('');
 	});
 	// SCROLL TO CALLBACK -END
 	
@@ -25,7 +29,7 @@ $(function(){
 	});
 	// -END
 
-	// ВАЛИДАЦИЯ ФОРМЫ FORM-CALLBACK
+	// ВАЛИДАЦИЯ ФОРМЫ (вверхняя форма)
 	$(".form-header").on('submit', function () {
 		var th = $(this);
 		let form = $(".form-header");
@@ -117,7 +121,9 @@ $(function(){
 					form.find('input').removeAttr('readonly');
 					formBtnSubmit.removeAttr('readonly');
 					th.trigger("reset");
-				}, 800);
+					// $('#form-cost__agree').prop('checked', true);
+					$('.form-header__input-btn input[type=submit]').addClass('off');
+				}, 5000);
 			}else{
 
 				// уберает показ ошибок
@@ -132,6 +138,7 @@ $(function(){
 					form.find('input').removeAttr('readonly');
 					formBtnSubmit.removeAttr('readonly');
 					th.trigger("reset");
+					$('#form-header__agree').prop('checked', true);
 				}, 800);
 			}
 		}).fail(function(response) {
@@ -148,11 +155,23 @@ $(function(){
 				form.find('input').removeAttr('readonly');
 				formBtnSubmit.removeAttr('readonly');
 				th.trigger("reset");
+				$('#form-header__agree').prop('checked', true);
 			}, 800);
 		});
 		return false;
 	});
-	//ВАЛИДАЦИЯ ФОРМЫ FORM-CALLBACK -END
+	//ВАЛИДАЦИЯ ФОРМЫ (вверхняя форма) -END
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -169,6 +188,138 @@ $(function(){
 		}
 	});
 	// -END
+
+
+	// ВАЛИДАЦИЯ ФОРМЫ (нижняя форма)
+	$(".form-cost").on('submit', function () {
+		var th = $(this);
+		let form = $(".form-cost");
+		let inputInfo = form.find('.form-cost__input-info');
+		let inputName = form.find('input[name="name"]');
+		let inputPhone = form.find('input[name="phone"]');
+		let formInfo = form.find('.form-cost__info');
+		let formBtnSubmit = form.find('input[type="submit"]');
+		var fail = false;
+
+		// (func) подбирает регулярное выражение по имени
+		function relesFunc(name, el) {
+			if (name == 'name') {
+				var regn = /^[a-zA-Zа-яёА-ЯЁ]{2,30}$/i;
+				return regn.test(el.val());
+			}
+			if (name == 'phone') {
+				var regp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+				return regp.test(el.val());
+			}
+			if (name == 'text') {
+				var regp = /^[a-zA-Zа-яёА-ЯЁ]{10,300}$/i;
+				return regp.test(el.val());
+			}
+		}
+		// (func) проверяет по регулярым выражения
+		function validateRegExp(varName, relesName, errorText1, errorText2) {
+			if (!varName.val()) {
+				varName.addClass('error');
+				fail = true;
+				varName.closest('.input-group').find('.input-info').html(errorText1);
+			} else if (!relesFunc(relesName, varName)) {
+				varName.addClass('error');
+				fail = true;
+				varName.closest('.input-group').find('.input-info').html(errorText2);
+			} else {
+				varName.removeClass('error');
+				varName.addClass('success');
+				varName.closest('.input-group').find('.input-info').html('');
+			}
+		}
+
+		//  очищает прошлое сообщение
+		formInfo.html('');
+		formBtnSubmit.val('Отправка').addClass('disabled-btn');
+		inputInfo.html('');
+
+		if( !$('#form-cost__agree').prop("checked") ){
+			fail = true;
+			console.log('вы не согласны!');
+		}
+
+		// проверка полей
+		validateRegExp(inputName, 'name', 'Обязательное поле! <span>*</span>', 'Некоректное Имя <span>*</span>');
+		validateRegExp(inputPhone, 'phone', 'Обязательное поле! <span>*</span>', 'Некоректный телефон <span>*</span>');
+
+		// вывод ошибки на экран
+		if (fail) {
+			formInfo.removeClass('info_acces');
+			formInfo.addClass('info_error');
+			formInfo.html('<p>Ошибка отправки формы, проверте поля на правильность. <span>*</span></p>');
+			formBtnSubmit.val('Узнать стоимость ремонта').removeClass('disabled-btn');
+			return false;
+		} else {
+			form.find('input').attr('readonly', '');
+			formBtnSubmit.attr('readonly', '');
+		}
+
+		// отправка формы
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function(response) {
+			console.log(response);
+			if( response === 'access' ){
+
+				// уберает показ ошибок
+				form.find('input').removeClass('error success');
+				formInfo.html('');
+				// показывает сообщение успешной отправки
+				formInfo.removeClass('info_error');
+				formInfo.addClass('info_acces');
+				setTimeout(function () {
+					formInfo.html('<p>Форма успешно отправлена, спасибо. В ближайшее время мы с Вами свяжемся.</p>');
+					formBtnSubmit.val('Узнать стоимость ремонта').removeClass('disabled-btn');
+					form.find('input').removeAttr('readonly');
+					formBtnSubmit.removeAttr('readonly');
+					th.trigger("reset");
+					// $('#form-cost__agree').prop('checked', true);
+					$('.form-cost__input-btn input[type=submit]').addClass('off');
+				}, 800);
+			}else{
+
+				// уберает показ ошибок
+				form.find('input').removeClass('error success');
+				formInfo.html('');
+				// показывает сообщение успешной отправки
+				formInfo.removeClass('info_acces');
+				formInfo.addClass('info_error');
+				setTimeout(function () {
+					formInfo.html('<p>Ошибка отправки формы!<span>*</span></p>');
+					formBtnSubmit.val('Узнать стоимость ремонта').removeClass('disabled-btn');
+					form.find('input').removeAttr('readonly');
+					formBtnSubmit.removeAttr('readonly');
+					th.trigger("reset");
+					$('#form-cost__agree').prop('checked', true);
+				}, 800);
+			}
+		}).fail(function(response) {
+			console.log(response);
+			// уберает показ ошибок
+			form.find('input').removeClass('error success');
+			formInfo.html('');
+			// показывает сообщение ошибки отправки
+			formInfo.removeClass('info_acces');
+			formInfo.addClass('info_error');
+			setTimeout(function () {
+				formInfo.html('<p>Ошибка отправки формы на сервер! Отправьте заявку позже. Приносим свои извинения. <span>*</span></p>');
+				formBtnSubmit.val('Узнать стоимость ремонта').removeClass('disabled-btn');
+				form.find('input').removeAttr('readonly');
+				formBtnSubmit.removeAttr('readonly');
+				th.trigger("reset");
+				$('#form-cost__agree').prop('checked', true);
+			}, 800);
+		});
+		return false;
+	});
+	//ВАЛИДАЦИЯ ФОРМЫ (нижняя форма) -END
 
 
 
--- mail.php
+++ mail.php
@@ -53,25 +53,25 @@ try {
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'mailphp9@gmail.com';                 // SMTP username
-    $mail->Password   = 'mailphp1234';                           // SMTP password
+    $mail->Username   = 'mailphp9@gmail.com';                   // SMTP username
+    $mail->Password   = 'mailphp1234';                          // SMTP password
     $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('zmeu.kostya@mail.ru');
-    $mail->addAddress('servise54@ya.ru');              // Add a recipient
-    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->setFrom('mailphp9@gmail.com');
+    $mail->addAddress('servise54@ya.ru');                       // Add a recipient
+    //$mail->addAddress('ellen@example.com');                   // Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
 
     // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');             // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');        // Optional name
 
     // Content
-    $mail->isHTML(true);                                    // Set email format to HTML
+    $mail->isHTML(true);                                        // Set email format to HTML
     $mail->Subject = 'Here is the subject';
     $mail->Body    = $message;
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
